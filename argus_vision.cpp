b8042799118057011cfd4abd5afb56057fd04517
James Alden <james.alden314159@gmail.com>
2026-02-25 20:32:47 +0300

    Update argus_vision.cpp

--- argus_vision.cpp
+++ argus_vision.cpp
@@ -4,14 +4,23 @@
 int main() {
 
     cv::VideoCapture cap(0);
-    cv::Mat webcam;
-    cv::Mat webcam_hsv,webcam_gray, webcam_blackwhite;
+    cv::Mat webcam_main;
+    cv::Mat webcam_hsv,webcam_gray, webcam_binary_mask;
 
+    int min_hue = 0, min_saturation = 0, min_value = 0;
+    int max_hue = 179, max_saturation = 255, max_value = 255;
     int target_x = 0, target_y = 0;
-   
+
+    cv::namedWindow("Argus X-1 Vision: Color Calibration");
+    cv::createTrackbar("Hue Min", "Argus X-1 Vision: Color Calibration", &min_hue, 179);
+    cv::createTrackbar("Hue Max", "Argus X-1 Vision: Color Calibration", &max_hue, 179); // max 179?
+    cv::createTrackbar("Sat Min", "Argus X-1 Vision: Color Calibration", &min_saturation, 255);
+    cv::createTrackbar("Sat Max", "Argus X-1 Vision: Color Calibration", &max_saturation, 255);
+    cv::createTrackbar("Val Min", "Argus X-1 Vision: Color Calibration", &min_value, 255);
+    cv::createTrackbar("Val Max", "Argus X-1 Vision: Color Calibration", &max_value, 255);
 
     if (!cap.isOpened()) {
-        std::cout << "Camera is not detected!" << '\n';
+        std::cout << "[ERROR] CAMERA NOT DETECTED!" << '\n';
 
         return -1;
 
@@ -19,29 +28,31 @@ int main() {
 
     while (true) {
 
-        cap >> webcam;
+        cap >> webcam_main;
 
-        cv::cvtColor(webcam, webcam_hsv, cv::COLOR_BGR2HSV);
-        cv::cvtColor(webcam, webcam_gray, cv::COLOR_BGR2GRAY);
-        cv::threshold(webcam_gray, webcam_blackwhite, 240, 255, cv::THRESH_BINARY); // gotta apply to grayscale, not hsv
+        cv::cvtColor(webcam_main, webcam_hsv, cv::COLOR_BGR2HSV);
 
-        cv::Moments m = cv::moments(webcam_blackwhite, true);
+        cv::Scalar lower_orange(min_hue, min_saturation, min_value);
+        cv::Scalar upper_orange(max_hue, max_saturation, max_value);
+
+        cv::inRange(webcam_hsv, lower_orange, upper_orange, webcam_binary_mask);
+        cv::Moments m = cv::moments(webcam_binary_mask, true);
 
         if (m.m00 > 200) { // 200 kinda good to filter noise, test more
 
             target_x = m.m10 / m.m00; // m00 = number of white pixels -- m10 = sum of x positions of white pixels -- m01 = sum of y positions of white pixels
             target_y = m.m01 / m.m00;
 
-            cv::circle(webcam, cv::Point(target_x, target_y), 5, cv::Scalar(0, 0, 255), -1);
+            cv::circle(webcam_main, cv::Point(target_x, target_y), 4, cv::Scalar(0, 0, 255), -1);
 
-            std::cout << "TARGET DETECTED AT -> X: " << target_x << " | Y: " << target_y << std::endl;
+            std::cout << "TARGET DETECTED AT -> X: " << target_x << " | Y: " << target_y << '\n'; // https://www.reddit.com/r/cpp_questions/comments/13i1282/stdendl_or_n/
         }
 
-        cv::imshow("Argus X-1 Vision Test: Main Camera", webcam);
-        cv::imshow("Argus X-1 Vision Test: Main Camera (HSV)", webcam_hsv);
-        cv::imshow("Argus X-1 Vision Test: Main Camera (Black & White", webcam_blackwhite); 
+        cv::imshow("Argus X-1 Vision: Main Source", webcam_main);
+        cv::imshow("Argus X-1 Vision: HSV Space", webcam_hsv);
+        cv::imshow("Argus X-1 Vision: Binary Mask", webcam_binary_mask);
 
-        if (webcam.empty())
+        if (webcam_main.empty())
         break;
 
         if (cv::waitKey(33) == 27) // 1000/33 = 30fps
