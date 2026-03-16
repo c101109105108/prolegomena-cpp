759f185a02c268b29dcc46b4c411be802aa5f793
Cemil U. Guven <cemilguven.research@gmail.com>
2026-03-17 02:26:04 +0300

    Update argus_state_machine.cpp

--- argus_state_machine.cpp
+++ argus_state_machine.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <cmath>
+#include <thread>
+#include <fstream>
+#include <pigpio.h>  
 
+enum ArgusState {
+
+IDLE
+SEARCH_T
+SEARCH_L
+SEARCH_TL
+FOUND_T
+FOUND_L
+FOUND_TL
+TRACK
+}
+
+int main() { 
+  ArgusState sleeeeeeeeepy_state = IDLE;
