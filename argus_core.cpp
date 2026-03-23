ff322de709efad89eada5de467db101d1f020bfe
Cemil U. Guven <cemilguven.research@gmail.com>
2026-03-23 13:21:17 +0300

    Update argus_core.cpp

--- argus_core.cpp
+++ argus_core.cpp
@@ -1,19 +1,27 @@
-// INTEGRAL WINDUP!
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2026 Cemil Uğurcan Güven
 
 #include "argus_core.h"
 
 #include <iostream>
 #include <cmath>
+#include <algorithm>
 
-const int center_pwm = 1365; // and/or 1355 tested center with mirror ds tape attached 10-15*x*x
-const int pwm_min = 520;
-const int pwm_max = 2520;
+const int center_pwm_x = 2042; 
+const int pwm_min_x = 1565;
+const int pwm_max_x = 2520;
+
+const int center_pwm_y = 1938; 
+const int pwm_min_y = 1355;
+const int pwm_max_y = 2520;
 
 const float proportional_gain = 0.12f; 
 const float integral_gain = 0.011f;     
 const float derivative_gain = 0.045f; 
 
 void CoreControl(ControlState& ctrl, float target_x, float target_y, float laser_x, float laser_y, double dt, float& output_x, float& output_y) {
+
+    const float windup_limit = 650.0f;
 
     float error_x = target_x - laser_x;
     float error_y = target_y - laser_y;
@@ -23,7 +31,9 @@ void CoreControl(ControlState& ctrl, float target_x, float target_y, float laser
 
     float p_x = proportional_gain * error_x;
     ctrl.integral_x += (error_x * dt);
+    ctrl.integral_x = std::clamp(ctrl.integral_x, -windup_limit, windup_limit);
     float i_x = integral_gain * ctrl.integral_x;
+    
     float d_x = 0.0f;
     if (dt > 0.0) {
         d_x = derivative_gain * ((error_x - ctrl.prev_error_x) / dt);
@@ -31,6 +41,7 @@ void CoreControl(ControlState& ctrl, float target_x, float target_y, float laser
 
     float p_y = proportional_gain * error_y;
     ctrl.integral_y += (error_y * dt);
+    ctrl.integral_y = std::clamp(ctrl.integral_y, -windup_limit, windup_limit);
     float i_y = integral_gain * ctrl.integral_y;
     float d_y = 0.0f;
     if (dt > 0.0) {
@@ -46,8 +57,8 @@ void CoreControl(ControlState& ctrl, float target_x, float target_y, float laser
     ctrl.pwm_y += output_y;
     ctrl.prev_error_y = error_y;
 
-    if (ctrl.pwm_x > pwm_max) ctrl.pwm_x = pwm_max;
-    if (ctrl.pwm_x < pwm_min) ctrl.pwm_x = pwm_min;
-    if (ctrl.pwm_y > pwm_max) ctrl.pwm_y = pwm_max;
-    if (ctrl.pwm_y < pwm_min) ctrl.pwm_y = pwm_min;
-}
+    if (ctrl.pwm_x > pwm_max_x) ctrl.pwm_x = pwm_max_x;
+    if (ctrl.pwm_x < pwm_min_x) ctrl.pwm_x = pwm_min_x;
+    if (ctrl.pwm_y > pwm_max_y) ctrl.pwm_y = pwm_max_y;
+    if (ctrl.pwm_y < pwm_min_y) ctrl.pwm_y = pwm_min_y;
+};
